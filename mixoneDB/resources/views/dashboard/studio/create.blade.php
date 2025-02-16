42a587fae657b2f0502ac60ad8ae4eef6650588a
EliasL94 <louhichielias94@gmail.com>
2025-02-16 18:57:57 +0100

    Gestion ajout d'image

--- mixoneDB/resources/views/dashboard/studio/create.blade.php
+++ mixoneDB/resources/views/dashboard/studio/create.blade.php
@@ -1,43 +1,36 @@
+To center the trash icon within its container, you can use CSS flexbox properties. Here is the updated code with the necessary CSS adjustments:
+
+```blade
 @extends('layouts.backendDB')
 
 @section('content')
 
     <div class="row y-gap-20 justify-between items-end pb-60 lg:pb-40 md:pb-32">
         <div class="col-auto">
-
             <h1 class="text-30 lh-14 fw-600">Ajouter un studio</h1>
             <div class="text-15 text-light-1">Lorem ipsum dolor sit amet, consectetur.</div>
-
         </div>
-
-        <div class="col-auto">
-
-        </div>
+        <div class="col-auto"></div>
     </div>
 
     <div class="py-30 px-30 rounded-4 bg-white shadow-3">
         <div class="tabs -underline-2 js-tabs">
             <div class="tabs__controls row x-gap-40 y-gap-10 lg:x-gap-20 js-tabs-controls">
-
                 <div class="col-auto">
                     <button class="tabs__button text-18 lg:text-16 text-light-1 fw-500 pb-5 lg:pb-0 js-tabs-button is-tab-el-active" data-tab-target=".-tab-item-1">1. Contenu</button>
                 </div>
-
                 <div class="col-auto">
                     <button class="tabs__button text-18 lg:text-16 text-light-1 fw-500 pb-5 lg:pb-0 js-tabs-button" data-tab-target=".-tab-item-2">2. Localisation</button>
                 </div>
-
                 <div class="col-auto">
                     <button class="tabs__button text-18 lg:text-16 text-light-1 fw-500 pb-5 lg:pb-0 js-tabs-button" data-tab-target=".-tab-item-3">3. Tarifs</button>
                 </div>
-
                 <div class="col-auto">
                     <button class="tabs__button text-18 lg:text-16 text-light-1 fw-500 pb-5 lg:pb-0 js-tabs-button" data-tab-target=".-tab-item-4">4. Attributs</button>
                 </div>
-
             </div>
 
-            <form method="post" action="{{ route('studio.store') }}">
+            <form method="post" action="{{ route('studio.store') }}" enctype="multipart/form-data">
                 @csrf
                 <div class="tabs__content pt-30 js-tabs-content">
                     <div class="tabs__pane -tab-item-1 is-tab-el-active">
@@ -80,28 +73,20 @@
                             </div>
                             <div class="mt-30">
                                 <div class="fw-500">Gallery</div>
-                                <div class="row x-gap-20 y-gap-20 pt-15">
+                                <div class="row x-gap-20 y-gap-20 pt-15" id="imagePreviewContainer">
                                     <div class="col-auto">
                                         <div class="w-200">
                                             <div class="d-flex ratio ratio-1:1">
-                                                <div class="flex-center flex-column text-center bg-blue-2 h-full w-1/1 absolute rounded-4 border-type-1">
+                                                <input type="file" id="imageUpload" name="images[]" accept="image/png, image/jpeg" onchange="previewImages(event)" multiple style="display: none;">
+                                                <label for="imageUpload" class="flex-center flex-column text-center bg-blue-2 h-full w-1/1 absolute rounded-4 border-type-1 cursor-pointer">
                                                     <div class="icon-upload-file text-40 text-blue-1 mb-10"></div>
                                                     <div class="text-blue-1 fw-500">Ajouter une image</div>
-                                                </div>
+                                                </label>
                                             </div>
                                             <div class="text-center mt-10 text-14 text-light-1">PNG ou JPG pas plus grand que 800px de hauteur et largeur.</div>
                                         </div>
                                     </div>
-                                    <div class="col-auto">
-                                        <div class="d-flex ratio ratio-1:1 w-200">
-                                            <img src="img/avatars/3.png" alt="image" class="img-ratio rounded-4">
-                                            <div class="d-flex justify-end px-10 py-10 h-100 w-1/1 absolute">
-                                                <div class="size-40 bg-white rounded-4">
-                                                    <i class="icon-trash text-16"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <!-- Image previews will be added here -->
                                 </div>
                             </div>
                         </div>
@@ -173,8 +158,7 @@
                                     <div class="form-input">
                                         <input type="text" name="min_hours" required>
                                         <label class="lh-1 text-16 text-light-1">Heures minimum</label>
-                                    </div>
-                                    @error('min_hours')
+                                    </div                                    @error('min_hours')
                                     <span class="text-danger" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -243,3 +227,58 @@
     </div>
 
 @endsection
+
+<style>
+    .img-ratio {
+        width: 100%;
+        height: auto;
+    }
+    .w-200 {
+        width: 200px;
+    }
+    .h-200 {
+        height: 200px;
+    }
+    .size-40 {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+<script>
+    function previewImages(event) {
+        const files = event.target.files;
+        const container = document.getElementById('imagePreviewContainer');
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            if (file.type === 'image/png' || file.type === 'image/jpeg') {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const div = document.createElement('div');
+                    div.classList.add('col-auto');
+                    div.innerHTML = `
+                        <div class="d-flex ratio ratio-1:1 w-200">
+                            <img src="${e.target.result}" alt="image" class="img-ratio rounded-4">
+                            <div class="d-flex justify-end px-10 py-10 h-100 w-1/1 absolute">
+                                <div class="size-40 bg-white rounded-4" onclick="removeImage(this)">
+                                    <i class="icon-trash text-16"></i>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                    container.insertBefore(div, container.children[1]);
+                };
+                reader.readAsDataURL(file);
+            } else {
+                alert('Only PNG and JPG images are allowed.');
+            }
+        }
+    }
+
+    function removeImage(element) {
+        element.closest('.col-auto').remove();
+    }
+</script>
+```
