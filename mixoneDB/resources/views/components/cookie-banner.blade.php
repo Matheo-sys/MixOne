cd6fabf61619f5099135d39c1f32aacb6d57cb57
EliasL94 <louhichielias94@gmail.com>
2026-05-11 19:41:01 +0200

    fix: uniformisation du design des boutons de la bannière cookie

--- mixoneDB/resources/views/components/cookie-banner.blade.php
+++ mixoneDB/resources/views/components/cookie-banner.blade.php
@@ -7,9 +7,9 @@
             </p>
         </div>
         <div class="col-xl-4 col-lg-3">
-            <div class="d-flex flex-wrap y-gap-10 x-gap-10 justify-center">
-                <button class="button -md bg-blue-1 text-white js-accept-cookies">Accepter</button>
-                <button class="button -md bg-light-2 text-dark-1 js-refuse-cookies">Refuser</button>
+            <div class="d-flex flex-wrap y-gap-10 x-gap-20 justify-end md:justify-center">
+                <button class="button -md bg-blue-1 text-white js-accept-cookies px-30" style="min-width: 130px;">Accepter</button>
+                <button class="button -md bg-light-2 text-dark-1 js-refuse-cookies px-30" style="min-width: 130px;">Refuser</button>
             </div>
         </div>
     </div>
